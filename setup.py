b6d19992533430b1ea6edbb5e6d5903d35494443
João Magalhães <joamag@gmail.com>
2022-12-05 15:42:02 +0000

    version: 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import setuptools
 
 setuptools.setup(
     name="battleship-py",
-    version="0.1.1",
+    version="0.1.2",
     author="João Magalhães",
     author_email="joamag@gmail.com",
     description="Simple Battleship game",
